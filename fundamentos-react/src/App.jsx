252806a69744fac992d59d7a07bed66667eb8d92
Felipe Alvares de Azevedo Macedo <felipeaamacedo@localhost.localdomain>
2020-06-09 23:03:46 -0300

    Criação do Card, Familia, FamiliaMembro e Estilo

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 
+import './App.css'
+import Card from './components/layout/Card'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Familia from './components/basicos/Familia'
 
 export default _ =>
-    <div>
-        <h1>Fundamentos React (Arrow_)</h1>
-        <Aleatorio 
-            maxNumber = {12} 
-            minNumber = {2}/>
-        <Fragmento />
-        <ComParametro
-            titulo='Situação Aluno' aluno='Pedro'
-            nota={9.3} />
-        <Primeiro></Primeiro>
+    <div className="App">
+        <h1>Fundamentos React</h1>
+        <div className="Cards">
+            <Card titulo='#05 - Componente com filhos' color='#85144b'>
+                <Familia sobrenome='Ferreira'></Familia>
+            </Card>
+
+
+        <Card titulo='#04 - Desafio Aleatório' color='#EB7260'>
+            <Aleatorio maxNumber = {12} minNumber = {2} />
+        </Card>
+
+        <Card titulo='#03 - Fragmento' color='#29ABA4'>
+            <Fragmento />
+        </Card>
+
+        <Card titulo='#02 - Com Parâmetro' color='#3A9AD9'>
+            <ComParametro
+                titulo='Situação Aluno' aluno='Pedro'
+                nota={9.3} />
+        </Card>
+
+        <Card titulo='#01 - Primeiro Componente' color='#354458'> 
+            <Primeiro></Primeiro>
+        </Card>
+        </div>
     </div>
