4d7108558ae2893aad5c007b0607d36e54d78111
Felipe Macedo <felipeaamacedo1@gmail.com>
2021-02-16 18:02:57 +0000

    Componente aleatorio optimizado

--- typescript/01_react-fundamentos/src/components/basicos/Aleatorio.tsx
+++ typescript/01_react-fundamentos/src/components/basicos/Aleatorio.tsx
@@ -12,12 +12,10 @@ function randomNumber(min:number, max:number):number{
 }
 
 export default (props:IProps):ReactElement=>{
-    let min:number = props.min
-    let max:number = props.max
     return(
         <div>
             <h2>Componente Aleatorio</h2>
-            <p> o número aleatório é <strong> { randomNumber(min, max)} </strong></p>
+            <p> o número aleatório é <strong> { randomNumber(props.min, props.max)} </strong></p>
         </div>
     )
 }
