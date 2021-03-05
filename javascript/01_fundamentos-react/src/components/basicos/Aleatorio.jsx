6d8da963c65d4353da0984440081ead4416b1c8f
Felipe Macedo <felipeaamacedo1@gmail.com>
2021-03-05 14:39:33 +0100

    init of react router project

--- javascript/01_fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ javascript/01_fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react"
 
 export default function Aleatorio(props) {
-    function randomNumber() {
-        let varRandomNumber = parseInt((Math.random() * (props.maxNumber - props.minNumber) + props.minNumber))
-
-        return document.getElementById('randomNumber').innerText = varRandomNumber
-    }
-    return (
-        <div>
-            <h1>Sorteando os números aleatórios!</h1>
-            <p>O número sorteado foi...</p>
-            <p id='randomNumber'>0</p>
-            <button onClick={randomNumber}>roll the dices</button>
-        </div>
+  function randomNumber() {
+    let varRandomNumber = parseInt(
+      Math.random() * (props.maxNumber - props.minNumber) + props.minNumber
     )
+
+    return (document.getElementById("randomNumber").innerText = varRandomNumber)
+  }
+  return (
+    <div>
+      <h1>Sorteando os números aleatórios!</h1>
+      <p>O número sorteado foi...</p>
+      <p id="randomNumber">0</p>
+      <button onClick={randomNumber}>roll the dices</button>
+    </div>
+  )
 }
--- javascript/01_fundamentos-react/src/components/repeticao/ListaAlunos.jsx
+++ javascript/01_fundamentos-react/src/components/repeticao/ListaAlunos.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import alunos from '../../data/alunos'
+import React from "react"
+import alunos from "../../data/alunos"
 
-
-export default props => {
-    const alunosLI = alunos.map((aluno) => {
-        return (
-            <li key={aluno.id}>
-                {aluno.id}) {aluno.nome} - {aluno.nota}
-            </li>
-        )
-    })
-
-
+export default (props) => {
+  const alunosLI = alunos.map((aluno) => {
     return (
-
-        <div>
-            <ul style={{ listStyle: 'none' }}>
-                {alunosLI}
-            </ul>
-        </div>
+      <li key={aluno.id}>
+        {aluno.id}) {aluno.nome} - {aluno.nota}
+      </li>
     )
+  })
+
+  return (
+    <div>
+      <ul style={{ listStyle: "none" }}>{alunosLI}</ul>
+    </div>
+  )
 }
